5a2b9b559ee1eee83e5e048000915942fa1607b5
Martin Sidorov <35573364+Matrix278@users.noreply.github.com>
2018-06-09 22:39:20 +0300

    Delete dbconnect.php

--- eesti-vene/dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "localhost";
-$user = "id2178577_martin";
-$psswd = "nitram27";
-$link = mysqli_connect($host, $user, $psswd);
-mysqli_select_db($link, "id2178577_translator");
-mysqli_query($link, "SET NAMES utf8");
-?>
